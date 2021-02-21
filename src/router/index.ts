8f6f745a094968f1f05d48280ac421d75cef0b2d
luyandan <2103029766@qq.com>
2021-02-21 10:03:04 +0800

    实现点击标签可跳转至编辑页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import statistics from '@/views/Statistics.vue'
+import EditLabel from '@/views/EditLabel.vue'
 import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter)
@@ -25,6 +26,10 @@ const routes: Array<RouteConfig> = [
   {
     path:'/statistics',
     component:statistics
+  },
+  {
+    path:'/labels/edit/:id',
+    component:EditLabel
   },
   {
     path:'*',
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      编辑标签
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator'
+import tagListModel from '@/models/tagListModel';
+
+@Component
+export default class EditLabel extends Vue{
+created(){
+    // console.log( this.$route.params)
+    const id=this.$route.params.id;
+    tagListModel.fetch();
+    const tags=tagListModel.data;
+    const tag=tags.filter(t=>t.id===id)[0]
+    if(tag){
+        console.log(tag)
+    }else{
+        this.$router.replace('/404')
+    }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
