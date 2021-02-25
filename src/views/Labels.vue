d5f46418794c04090651257413a00f1de3d26c3d
luyandan <2103029766@qq.com>
2021-02-25 13:59:52 +0800

    将computed改为get

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,70 +1,72 @@
 <template>
-<Layout>
+  <Layout>
     <div class="tags">
-        <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}/`">
-            <span>{{tag.name}}</span> 
-         <Icon name="right"/>
-        </router-link>
+      <router-link
+        class="tag"
+        v-for="tag in tags"
+        :key="tag.id"
+        :to="`/labels/edit/${tag.id}/`"
+      >
+        <span>{{ tag.name }}</span>
+        <Icon name="right" />
+      </router-link>
     </div>
-        <div class="createTag-wrapper">
-            <Button class="createTag" @click="createTag">新建标签</Button>
-        </div>
-</Layout>
+    <div class="createTag-wrapper">
+      <Button class="createTag" @click="createTag">新建标签</Button>
+    </div>
+  </Layout>
 </template>
 <script lang="ts">
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
-import Button from '@/components/Button.vue';
-import {mixins} from 'vue-class-component'
-import TagHelper from '@/mixins/TagHelper';
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Button from "@/components/Button.vue";
+import { mixins } from "vue-class-component";
+import TagHelper from "@/mixins/TagHelper";
 
 @Component({
-    components:{Button},
-    computed: {
-      tags(){
-        return this.$store.state.tagList
-      }
-    }
+  components: { Button },
 })
-export default class Labels extends mixins(TagHelper){
-    beforeCreate(){
-      this.$store.commit('fetchTags')
-    }
-
+export default class Labels extends mixins(TagHelper) {
+  get tags() {
+    return this.$store.state.tagList;
+  }
+  beforeCreate() {
+    this.$store.commit("fetchTags");
+  }
 }
 </script>
 
 <style lang="scss" scoped>
- .tags{
-     background: white;
-     font-size: 16px;
-     padding-left:16px ;
-     >.tag{
-         min-height: 44px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      border-bottom: 1px solid #e6e6e6;
-        svg{
-         width: 18px;
-         height: 18px;
-         color: #666;
-         margin-right: 16px;
-     }
-     }
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+  > .tag {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
     }
-  .createTag{   
-    background: #767676;
-    color: white;
-    border-radius: 4px;
-    border: none;
-    height: 40px;
-    padding: 0 16px;
-    &-wrapper{ //&-wrapper是createTag的父元素
+  }
+}
+.createTag {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper {
+    //&-wrapper是createTag的父元素
     text-align: center;
     padding: 16px;
     margin-top: 28px;
-    }
   }
- 
-</style> 
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,68 +1,69 @@
 <template>
-    <Layout class-prefix="layout">
-       <NumberPad :value.sync="record.amount" @submit="saveRecord" />
-       <!-- <Types :value="record.type" @update:value="onUpdateType"/> -->
-        <Types :value.sync="record.type"/>
-        <div class="notes">
-       <FormItem field-name="备注" 
-              placeholder="在这里输入备注"
-              @update:value="onUpdateNotes"/>
-       </div>
-       <Tags/>
-    </Layout>
+  <Layout class-prefix="layout">
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+    <!-- <Types :value="record.type" @update:value="onUpdateType"/> -->
+    <Types :value.sync="record.type" />
+    <div class="notes">
+      <FormItem
+        field-name="备注"
+        placeholder="在这里输入备注"
+        @update:value="onUpdateNotes"
+      />
+    </div>
+    <Tags />
+  </Layout>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import NumberPad from "@/components/Money/NumberPad.vue"
-import Types from "@/components/Money/Types.vue"
-import FormItem from "@/components/Money/FormItem.vue"
-import Tags from "@/components/Money/Tags.vue"
-import {Component} from 'vue-property-decorator'
-
-
+import Vue from "vue";
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import FormItem from "@/components/Money/FormItem.vue";
+import Tags from "@/components/Money/Tags.vue";
+import { Component } from "vue-property-decorator";
 
 @Component({
-components:{NumberPad,Types,FormItem,Tags},
-computed: {
-recordList(){
-   return this.$store.state.recordList;
-  }
-}
+  components: { NumberPad, Types, FormItem, Tags },
 })
-export default class Money extends Vue{
- record: RecordItem={
-    tags:[],notes:'',type: '-',amount:0
-}
+export default class Money extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+  record: RecordItem = {
+    tags: [],
+    notes: "",
+    type: "-",
+    amount: 0,
+  };
 
-created(){
-  this.$store.commit('fetchRecords')
-}
-onUpdateNotes(value: string){
+  created() {
+    this.$store.commit("fetchRecords");
+  }
+  onUpdateNotes(value: string) {
     // console.log(value);
- this.record.notes = value
-}
-// onUpdateType(value: string){
-//     // console.log(value)
-//     this.record.type=value
-// }
-onUpdateAmount(value: string){
-//   console.log(value)
-  this.record.amount=parseFloat(value)
-}
-saveRecord(){
-  //  store.createRecord(this.record)
-  this.$store.commit('createRecord',this.record)
-}
+    this.record.notes = value;
+  }
+  // onUpdateType(value: string){
+  //     // console.log(value)
+  //     this.record.type=value
+  // }
+  onUpdateAmount(value: string) {
+    //   console.log(value)
+    this.record.amount = parseFloat(value);
+  }
+  saveRecord() {
+    //  store.createRecord(this.record)
+    this.$store.commit("createRecord", this.record);
+  }
 }
 </script>
 
 <style lang="scss">
-.layout-content{
-    display: flex;
-    flex-direction: column-reverse;
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
 }
-  .notes{
-    padding: 12px 0;
-  }
-</style> 
+.notes {
+  padding: 12px 0;
+}
+</style>
